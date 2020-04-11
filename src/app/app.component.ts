18913c42b2152d2269ee314a72b997cd3ad6789c
Blackscure <Wekesabuyahi@gmail.com>
2020-04-11 15:21:44 +0300

    --- Auto-Git Commit ---

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: '<h1>First Angular 2 App<github></github></h1>',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent {
-  
-}
+export class AppComponent  { name = 'Angular'; }
