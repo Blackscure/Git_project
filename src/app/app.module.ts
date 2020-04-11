677491db6f8218500af85a33c8d1698c1805ba49
Blackscure <Wekesabuyahi@gmail.com>
2020-04-11 10:16:26 +0300

    --- Auto-Git Commit ---

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
     NavbarComponent
   ],
   imports: [
