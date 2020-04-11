383d112cf9027dad7ae187e6159cfe596b4dbe9b
Blackscure <Wekesabuyahi@gmail.com>
2020-04-11 19:30:16 +0300

    --- Auto-Git Commit ---

--- src/app/components/github.components.ts
+++ src/app/components/github.components.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'github',
   template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+    constructor() {
+        console.log('Github Component Int...');
+    }
+}
