7186d8e46afba2e3bbf81efc57abd618b73284e6
Blackscure <Wekesabuyahi@gmail.com>
2020-04-11 11:16:46 +0300

    --- Auto-Git Commit ---

--- src/app/components/github.components .ts
+++ src/app/components/github.components .ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'gitApp',
-  templateUrl: '<h1>My First Angualar app</h1>'
+  selector: 'github',
+  templateUrl: 'GITHUB COMPONENT'
 })
 export class GithubComponent {
   constructor(){
